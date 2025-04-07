63eba241d726bf537ba67ed41e03a23c09abd123
Ozod1234456778 <ozod4115@gmail.com>
2025-04-07 15:05:35 +0500

    Update lesson.py

--- lesson-1/homework/lesson.py
+++ lesson-1/homework/lesson.py
@@ -1,1 +1,20 @@
-tfu6tftyg
+1:Given a side of square. Find its perimeter and area
+side = 5
+print(4 * side)
+print(side ** 2)
+
+2:Given diameter of circle. Find its length.
+diametr = 10
+pi = 3.141559
+print(pi * diametr)
+
+3:Given two numbers a and b. Find their mean.
+a= 7
+b = 13 
+print((a + b) / 2)
+
+4:Given two numbers a and b. Find their sum, product and square of each number.
+print(a + b)
+print(a * b)
+print(a ** 2)
+print(b ** 2)
